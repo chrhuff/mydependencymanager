173e1639cccf4b24dc79cb44395d950c32c7de5e
Christian Huff <christian.huff@cofinpro.de>
2017-05-31 17:49:18 +0200

    Added dummy dashboard.

--- mdm/src/main/java/de/genohackathon/mdm/frontend/views/DashboardView.java
+++ mdm/src/main/java/de/genohackathon/mdm/frontend/views/DashboardView.java
@@ -2,13 +2,20 @@ package de.genohackathon.mdm.frontend.views;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.server.ClassResource;
+import com.vaadin.ui.Image;
 import com.vaadin.ui.VerticalLayout;
 
 /**
  * Created by chuff on 31.05.2017.
  */
 public class DashboardView extends VerticalLayout implements View {
-    
+
+    public DashboardView() {
+        Image image = new Image("Dashboard", new ClassResource("/dashboard.png"));
+        addComponent(image);
+    }
+
     @Override
     public void enter(ViewChangeListener.ViewChangeEvent event) {
     }
